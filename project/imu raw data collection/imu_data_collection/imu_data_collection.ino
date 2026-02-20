23bbd3030ba69670b73ce854386a090b46033516
KohMingLiang <mingliang140@gmail.com>
2026-02-20 12:50:59 +0100

    imu raw data

---
+++ project/imu raw data collection/imu_data_collection/imu_data_collection.ino
@@ -0,0 +1,269 @@
+#include <Wire.h>
+/****************************************************************
+ * Example1_Basics.ino
+ * ICM 20948 Arduino Library Demo
+ * Use the default configuration to stream 9-axis IMU data
+ * Owen Lyke @ SparkFun Electronics
+ * Original Creation Date: April 17 2019
+ *
+ * Please see License.md for the license information.
+ *
+ * Distributed as-is; no warranty is given.
+ ***************************************************************/
+#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+
+//#define USE_SPI       // Uncomment this to use SPI
+
+#define SERIAL_PORT Serial
+
+#define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
+#define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
+
+#define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
+// The value of the last bit of the I2C address.
+// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define AD0_VAL 1 
+
+#ifdef USE_SPI
+ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
+#else
+ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
+#endif
+
+void setup()
+{
+
+  SERIAL_PORT.begin(115200);
+  while (!SERIAL_PORT)
+  {
+  };
+
+#ifdef USE_SPI
+  SPI_PORT.begin();
+#else
+  WIRE_PORT.begin(2,1,10000);
+  //WIRE_PORT.setClock(400000);
+#endif
+
+  myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+
+  bool initialized = false;
+  while (!initialized)
+  {
+
+#ifdef USE_SPI
+    myICM.begin(CS_PIN, SPI_PORT);
+#else
+    myICM.begin(WIRE_PORT, AD0_VAL);
+#endif
+
+    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
+      SERIAL_PORT.println("Trying again...");
+      delay(500);
+    }
+    else
+    {
+      initialized = true;
+    }
+  }
+}
+
+void loop()
+{
+
+  if (myICM.dataReady())
+  {
+    delay(500);
+    myICM.getAGMT();                        // The values are only updated when you call 'getAGMT'
+    //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
+    printScaledAGMT(&myICM);            // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+    //printTilt(&myICM);
+    //printVibration(&myICM);
+    delay(30);
+  }
+  else
+  {
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
+}
+
+// Below here are some helper functions to print the data nicely!
+
+void printPaddedInt16b(int16_t val)
+{
+  if (val > 0)
+  {
+    SERIAL_PORT.print(" ");
+    if (val < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  else
+  {
+    SERIAL_PORT.print("-");
+    if (abs(val) < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  SERIAL_PORT.print(abs(val));
+}
+
+void printRawAGMT(ICM_20948_AGMT_t agmt)
+{
+  SERIAL_PORT.print("RAW. Acc [ "); // Linear motion and Gravity. 1000mg is equal to 1g (Earth's gravity)
+  printPaddedInt16b(agmt.acc.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.z);
+  SERIAL_PORT.print(" ], Gyr [ "); // Rotational speed. This tells you how fast the sensor is spinning around its axes (degree/sec)
+  printPaddedInt16b(agmt.gyr.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.z);
+  SERIAL_PORT.print(" ], Mag [ "); // Magnetic fields. This detects the Earth's magnetic field (like a compass) or nearby magnets and metal objects (uT) micro Tesla
+  printPaddedInt16b(agmt.mag.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.z);
+  SERIAL_PORT.print(" ], Tmp [ "); // temperature in degree celsius
+  printPaddedInt16b(agmt.tmp.val);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
+
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+  float aval = abs(val);
+  if (val < 0)
+  {
+    SERIAL_PORT.print("-");
+  }
+  else
+  {
+    SERIAL_PORT.print(" ");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++)
+  {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1))
+    {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+    {
+      tenpow *= 10;
+    }
+    if (aval < tenpow)
+    {
+      SERIAL_PORT.print("0");
+    }
+    else
+    {
+      break;
+    }
+  }
+  if (val < 0)
+  {
+    SERIAL_PORT.print(-val, decimals);
+  }
+  else
+  {
+    SERIAL_PORT.print(val, decimals);
+  }
+}
+
+#ifdef USE_SPI
+void printScaledAGMT(ICM_20948_SPI *sensor)
+{
+#else
+void printScaledAGMT(ICM_20948_I2C *sensor)
+{
+#endif
+  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+  printFormattedFloat(sensor->accX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accZ(), 5, 2);
+  // SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+  // printFormattedFloat(sensor->gyrX(), 5, 2);
+  // SERIAL_PORT.print(", ");
+  // printFormattedFloat(sensor->gyrY(), 5, 2);
+  // SERIAL_PORT.print(", ");
+  // printFormattedFloat(sensor->gyrZ(), 5, 2);
+  // SERIAL_PORT.print(" ], Mag (uT) [ ");
+  // printFormattedFloat(sensor->magX(), 5, 2);
+  // SERIAL_PORT.print(", ");
+  // printFormattedFloat(sensor->magY(), 5, 2);
+  // SERIAL_PORT.print(", ");
+  // printFormattedFloat(sensor->magZ(), 5, 2);
+  // SERIAL_PORT.print(" ], Tmp (C) [ ");
+  // printFormattedFloat(sensor->temp(), 5, 2);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
+
+void printTilt(ICM_20948_I2C *sensor) // ML
+{
+  float x = sensor->accX(); // 
+  float y = sensor->accY(); // 
+  float z = sensor->accZ(); // 
+
+  // Calculate Pitch and Roll in degrees
+  float pitch = atan2(y, z) * 180 / M_PI;
+  float roll = atan2(-x, sqrt(y*y + z*z)) * 180 / M_PI;
+
+  SERIAL_PORT.print("Tilt (Deg): Pitch: ");
+  SERIAL_PORT.print(pitch);
+  SERIAL_PORT.print(" | Roll: ");
+  SERIAL_PORT.println(roll);
+}
+
+void printVibration(ICM_20948_I2C *sensor) // ML
+{
+  float x = sensor->accX(); 
+  float y = sensor->accY(); 
+  float z = sensor->accZ(); 
+
+  // Calculate the total magnitude of acceleration
+  float totalAcc = sqrt((x * x) + (y * y) + (z * z));
+
+  // Subtract Earth's gravity (approx 1000mg) to get the "extra" vibration/motion
+  float vibration = abs(totalAcc - 1000.0);
+
+  SERIAL_PORT.print("Vibration Intensity (mg): ");
+  SERIAL_PORT.println(vibration);
+}
