8f6c406a5e79d1fdd4b49d62e702aa4f3188660f
Wangshuai Zeng <wzeng@kth.se>
2026-02-23 19:17:52 +0100

    add train data

--- Client/Client.ino
+++ Client/Client.ino
@@ -27,7 +27,7 @@ const char* firebaseUrl =
 
 BLEClientHandler bleClient;
 
-bool debug = true;
+bool debug = false;
 
 unsigned long previousMillis = 0;
 
@@ -66,7 +66,7 @@ void loop() {
 
    if(ensureWiFiConnected(ssid, password)){
 
-        if (env.floor != lastFloor) {
+        if (env.floor != lastFloor||) {
             lastFloor = env.floor;       
             lastPattern = env.pattern;  
 
